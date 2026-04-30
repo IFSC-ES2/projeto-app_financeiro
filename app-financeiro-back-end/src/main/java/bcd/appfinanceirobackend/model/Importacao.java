29c64d58456b2204972bc2c564915869cacb8592
Victor3294 <vblum26@gmail.com>
2026-04-29 21:47:23 -0300

    feat: Implementando os models Importação e Fatura

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/model/Importacao.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/model/Importacao.java
@@ -1,4 +1,45 @@
 package bcd.appfinanceirobackend.model;
 
+import bcd.appfinanceirobackend.model.enums.FormatoArquivo;
+import bcd.appfinanceirobackend.model.enums.StatusImportacao;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
 public class Importacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    @OneToMany(mappedBy = "importacao")
+    private List<Transacao> transacoes;
+
+    @Column(nullable = false)
+    private String nome_arquivo;
+
+    @Column(nullable = false)
+    private FormatoArquivo formatoArquivo;
+
+    @Column(nullable = false)
+    private StatusImportacao statusImportacao;
+
+    @Column(nullable = false)
+    private LocalDateTime importado_em;
+
+    @Column
+    private int total_linhas;
+
+    @Column
+    private int sucessos;
+
+    @Column
+    private int falhas;
 }
