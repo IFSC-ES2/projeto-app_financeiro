75b8f2077680d9b959f3767a2f831120349f5ce1
jpifsc <joao.pg2006@aluno.ifsc.edu.br>
2026-05-03 16:20:46 -0300

    fix: corrige exportação do componente Home e imports no App

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/security/WebConfig.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/security/WebConfig.java
@@ -1,4 +1,4 @@
-package bcd.appfinanceiro.security;
+package bcd.appfinanceirobackend.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173") // URL do seu front-end Vite
+                .allowedOrigins("http://localhost:5173") // URL front-end Vite
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
