0608890a9686bb24db5ab51f2c5358335257a4fb
apvillela <ap.villela10@gmail.com>
2026-05-05 00:01:13 -0300

    feat(tokendto): preenche dtos de auth (register, login, token)

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/auth/TokenDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/auth/TokenDTO.java
@@ -1,4 +1,14 @@
 package bcd.appfinanceirobackend.dto.auth;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public class TokenDTO {
+    private String accessToken;
+    private String tipo;
+    private LocalDateTime expiracao;
 }
