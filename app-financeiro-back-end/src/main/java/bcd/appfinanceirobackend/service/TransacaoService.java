cc87a90b02acd131915bbe7ade20b117aa40f6f8
Victor3294 <vblum26@gmail.com>
2026-05-12 21:29:25 -0300

    feat: Adicionando o conteúdo do TransacaoRequestDTO e começando o service

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
@@ -1,4 +1,14 @@
 package bcd.appfinanceirobackend.service;
 
+import bcd.appfinanceirobackend.dto.transacao.TransacaoRequestDTO;
+import bcd.appfinanceirobackend.dto.transacao.TransacaoResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransacaoService {
+
+    public TransacaoResponseDTO registrarManual (TransacaoRequestDTO dto) {
+        
+    }
+
 }
