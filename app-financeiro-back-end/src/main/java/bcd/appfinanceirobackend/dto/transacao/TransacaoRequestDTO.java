725fcd54236597094e2afee9ade15acff4b5ba4a
Victor3294 <vblum26@gmail.com>
2026-05-12 21:29:25 -0300

    feat: Adicionando o conteúdo do TransacaoRequestDTO e começando o service

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/transacao/TransacaoRequestDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/transacao/TransacaoRequestDTO.java
@@ -1,4 +1,18 @@
 package bcd.appfinanceirobackend.dto.transacao;
 
+import bcd.appfinanceirobackend.model.enums.TipoPagamento;
+import bcd.appfinanceirobackend.model.enums.TipoTransacao;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class TransacaoRequestDTO {
+    private BigDecimal valor;
+    private LocalDateTime data;
+    private String descricao;
+    private TipoTransacao tipoTransacao;
+    private TipoPagamento formaPagamento;
+    private UUID categoriaId;
+    private UUID contaId;
 }
