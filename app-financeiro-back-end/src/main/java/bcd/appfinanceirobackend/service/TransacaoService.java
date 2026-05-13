cd63ae16709c98c5d5df6137225c8fd33944a8bd
Victor3294 <vblum26@gmail.com>
2026-05-12 21:49:21 -0300

    feat: Estruturando o TransacaoRequest, começo da função de registrarManual no TransacaoService e ajustes pontuais
    Os ajustes foram a correção dos repositorys para interfaces

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
@@ -1,4 +1,22 @@
 package bcd.appfinanceirobackend.service;
 
+import bcd.appfinanceirobackend.dto.transacao.TransacaoRequestDTO;
+import bcd.appfinanceirobackend.dto.transacao.TransacaoResponseDTO;
+import bcd.appfinanceirobackend.model.Transacao;
+import bcd.appfinanceirobackend.repository.ContaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TransacaoService {
+
+    public TransacaoResponseDTO registrarManual (TransacaoRequestDTO dto) {
+        if(dto.getValor() == null ||
+                dto.getContaId() == null ||
+                dto.getData() == null) throw new IllegalArgumentException("Campos obrigatórios não informados");
+
+        Transacao transacao = new Transacao();
+        transacao.setCategorizada(true);
+        transacao.setConta(ContaRepository. dto.getContaId());
+    }
+
 }
