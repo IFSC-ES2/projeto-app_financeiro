581e0f5d97b213b6d4b1adf2b92ef0ffdbf5a8bc
Victor3294 <vblum26@gmail.com>
2026-05-13 15:02:26 -0300

    feat: Criando o endpoint transacoes/maual

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/controller/TransacaoController.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/controller/TransacaoController.java
@@ -1,4 +1,26 @@
 package bcd.appfinanceirobackend.controller;
 
+import bcd.appfinanceirobackend.dto.transacao.TransacaoRequestDTO;
+import bcd.appfinanceirobackend.dto.transacao.TransacaoResponseDTO;
+import bcd.appfinanceirobackend.service.TransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transacoes")
 public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController( TransacaoService transacaoService) {
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping("/manual")
+    public ResponseEntity<TransacaoResponseDTO> registrarTransacaoManual (TransacaoRequestDTO requestDTO) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.registrarManual(requestDTO));
+    }
 }
