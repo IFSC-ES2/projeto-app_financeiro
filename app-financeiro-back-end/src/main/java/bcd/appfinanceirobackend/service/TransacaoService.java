276469f2f1814a4accad4b1ddcdca43316309ccc
Victor3294 <vblum26@gmail.com>
2026-05-12 22:16:39 -0300

    feat: Inicio do registro de transacaoManual e alguns ajustes pontuais

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/TransacaoService.java
@@ -2,21 +2,46 @@ package bcd.appfinanceirobackend.service;
 
 import bcd.appfinanceirobackend.dto.transacao.TransacaoRequestDTO;
 import bcd.appfinanceirobackend.dto.transacao.TransacaoResponseDTO;
+import bcd.appfinanceirobackend.model.Conta;
 import bcd.appfinanceirobackend.model.Transacao;
 import bcd.appfinanceirobackend.repository.ContaRepository;
+import bcd.appfinanceirobackend.repository.TransacaoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TransacaoService {
+
+    private TransacaoRepository transacaoRepository;
+    private ContaRepository contaRepository;
+
+    public TransacaoService(TransacaoRepository transacaoRepository, ContaRepository contaRepository) {
+        this.transacaoRepository = transacaoRepository;
+        this.contaRepository = contaRepository;
+    }
 
     public TransacaoResponseDTO registrarManual (TransacaoRequestDTO dto) {
         if(dto.getValor() == null ||
                 dto.getContaId() == null ||
                 dto.getData() == null) throw new IllegalArgumentException("Campos obrigatórios não informados");
 
+        Optional<Conta> conta = contaRepository.findById(dto.getContaId());
+
         Transacao transacao = new Transacao();
         transacao.setCategorizada(true);
-        transacao.setConta(ContaRepository. dto.getContaId());
+        transacao.setConta(conta);
+        transacao.setValor(dto.getValor());
+        transacao.setData(dto.getData());
+        transacao.setDescricao(dto.getDescricao());
+        transacao.setTipo(dto.getTipoTransacao());
+        transacao.setFormaPagamento(dto.getFormaPagamento());
+        transacaoRepository.save(transacao);
+
+    }
+
+    public TransacaoResponseDTO toResponse(Transacao transacao) {
+        
     }
 
 }
