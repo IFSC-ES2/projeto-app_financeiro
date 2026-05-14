b7e6e7f3dfa2da17faf38276f11748e8d934116e
Victor3294 <vblum26@gmail.com>
2026-05-14 18:56:02 -0300

    fix: Aplicando correção na importação do projeto

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/config/JwtAuthFilter.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/config/JwtAuthFilter.java
@@ -6,7 +6,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -29,8 +28,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
-                                    @NonNull HttpServletResponse response,
-                                    @NonNull FilterChain filterChain)
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
             throws ServletException, IOException {
 
         String header = request.getHeader("Authorization");
