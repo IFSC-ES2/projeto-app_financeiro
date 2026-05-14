945cfbc532ad3d37e722566f5d48f9bbb155bfb1
Victor3294 <vblum26@gmail.com>
2026-05-14 19:29:57 -0300

    feat: Criando os DTOS da Conta

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/ContaService.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/ContaService.java
@@ -1,4 +1,10 @@
 package bcd.appfinanceirobackend.service;
 
+import bcd.appfinanceirobackend.dto.conta.ContaResponseDTO;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ContaService   {
+
+    public ContaResponseDTO
 }
