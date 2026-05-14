cce3fd5db9ccf515fb354e3136187a04a5626699
Victor3294 <vblum26@gmail.com>
2026-05-14 19:29:57 -0300

    feat: Criando os DTOS da Conta

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/conta/ContaResponseDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/conta/ContaResponseDTO.java
@@ -1,4 +1,17 @@
 package bcd.appfinanceirobackend.dto.conta;
 
+import bcd.appfinanceirobackend.model.enums.TipoConta;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
 public class ContaResponseDTO {
+    private UUID contaId;
+    private String nome;
+    private TipoConta tipoConta;
+    private String banco;
+    private String descricao;
 }
