2d383b0f4e7a343a8bd797797872efa8b6ac40fa
Victor Lacerda <victor.g03@aluno.ifsc.edu.br>
2026-05-16 17:14:06 -0300

    fix: adiciona campos e setters no DTO de categoria

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/transacao/CategoriaTransacaoDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/transacao/CategoriaTransacaoDTO.java
@@ -1,4 +1,52 @@
 package bcd.appfinanceirobackend.dto.transacao;
 
+import java.util.UUID;
+
 public class CategoriaTransacaoDTO {
+
+    private UUID categoriaId;
+    private String nome;
+    private String icone;
+    private String cor;
+    private boolean padrao;
+
+    public UUID getCategoriaId() {
+        return categoriaId;
+    }
+
+    public void setCategoriaId(UUID categoriaId) {
+        this.categoriaId = categoriaId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIcone() {
+        return icone;
+    }
+
+    public void setIcone(String icone) {
+        this.icone = icone;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isPadrao() {
+        return padrao;
+    }
+
+    public void setPadrao(boolean padrao) {
+        this.padrao = padrao;
+    }
 }
