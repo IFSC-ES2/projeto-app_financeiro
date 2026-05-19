53cd02faa98c6bd5b8282bc6016800cac6fef064
Victor3294 <vblum26@gmail.com>
2026-05-19 19:03:55 -0300

    feat: Finalizando o ImportaçãoService

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/ImportacaoService.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/service/ImportacaoService.java
@@ -1,16 +1,22 @@
 package bcd.appfinanceirobackend.service;
 
 import bcd.appfinanceirobackend.dto.importacao.ImportacaoResponseDTO;
+import bcd.appfinanceirobackend.exception.ResourceNotFoundException;
+import bcd.appfinanceirobackend.model.Conta;
 import bcd.appfinanceirobackend.model.Importacao;
+import bcd.appfinanceirobackend.model.Transacao;
 import bcd.appfinanceirobackend.model.Usuario;
 import bcd.appfinanceirobackend.model.enums.StatusImportacao;
 import bcd.appfinanceirobackend.parser.ParserExtrato;
 import bcd.appfinanceirobackend.repository.ContaRepository;
 import bcd.appfinanceirobackend.repository.ImportacaoRepository;
 import bcd.appfinanceirobackend.repository.TransacaoRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -35,11 +41,53 @@ public class ImportacaoService {
     public ImportacaoResponseDTO processar(MultipartFile arquivo,
                                            UUID contaId,
                                            Usuario usuarioAutenticado) {
-        
+
+        Conta conta = contaRepository.findById(contaId)
+                .orElseThrow(() -> new ResourceNotFoundException("Conta não encontrada"));
+        Importacao importacao = new Importacao();
+        importacao.setUsuario(usuarioAutenticado);
+        importacao.setStatusImportacao(StatusImportacao.PENDENTE);
+        importacao.setImportado_em(LocalDateTime.now());
+        importacaoRepository.save(importacao);
+
+        ParserExtrato parser = parsers.stream()
+                .filter(p -> p.aceita(arquivo))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(
+                        HttpStatus.BAD_REQUEST, "Formato de arquivo não suportado"));
+
+        importacao.setStatusImportacao(StatusImportacao.PROCESSANDO);
+        importacaoRepository.save(importacao);
+
+        int sucessos = 0, falhas = 0;
+        try {
+            List<Transacao> transacoes = parser.parsear(arquivo, conta);
+            for (Transacao t : transacoes) {
+                try {
+                    t.setImportacao(importacao);
+                    t.setCategorizada(false);
+                    transacaoRepository.save(t);
+                    sucessos++;
+                } catch (Exception e) {
+                    falhas++;
+                }
+            }
+            importacao.setStatusImportacao(StatusImportacao.CONCLUIDO);
+        } catch (Exception e) {
+            importacao.setStatusImportacao(StatusImportacao.ERRO);
+        }
+
+        importacao.setSucessos(sucessos);
+        importacao.setFalhas(falhas);
+        importacaoRepository.save(importacao);
+
+        return toResponse(importacao);
     }
 
-    public StatusImportacao buscarStatus() {
-
+    public StatusImportacao buscarStatus(UUID importacaoID) {
+        Importacao importacao = importacaoRepository.findById(importacaoID).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Importação não encontrada"));
+        return importacao.getStatusImportacao();
     }
 
 
