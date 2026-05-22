395dd0d1b8551b28418af79b06dd4a5bba0d293a
Victor3294 <vblum26@gmail.com>
2026-05-22 14:40:17 -0300

    feat: Criando o endpoint de categorização manual + iniciando configuração das migrations com flyway

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/controller/TransacaoController.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/controller/TransacaoController.java
@@ -7,10 +7,9 @@ import bcd.appfinanceirobackend.service.TransacaoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/transacoes")
@@ -28,5 +27,15 @@ public class TransacaoController {
             @AuthenticationPrincipal Usuario usuarioAutenticado) {
         return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.registrarManual(
                 requestDTO, usuarioAutenticado));
+    }
+
+    @PatchMapping("/{transacaoId}/categoria")
+    public ResponseEntity<TransacaoResponseDTO> categorizarTransacaoManual (
+            @PathVariable UUID transacaoId,
+            @RequestBody UUID categoriaId,
+            @AuthenticationPrincipal Usuario usuarioAutenticado) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.categorizar(
+                transacaoId, categoriaId, usuarioAutenticado
+        ));
     }
 }
