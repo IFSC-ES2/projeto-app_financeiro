d7bcc8db516fb7c121d36db6e4a2991f51dbaec1
alexandre.villela <ap.villela10@gmail.com>
2026-05-30 23:53:37 -0300

    add server-side pagination and filters to transactions endpoint (#106)

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/TransacaoRepository.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/TransacaoRepository.java
@@ -2,12 +2,12 @@ package bcd.appfinanceirobackend.repository;
 
 import bcd.appfinanceirobackend.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
-    List<Transacao> findAllByContaUsuarioId(UUID usuarioId);
+public interface TransacaoRepository
+        extends JpaRepository<Transacao, UUID>, JpaSpecificationExecutor<Transacao> {
 }
