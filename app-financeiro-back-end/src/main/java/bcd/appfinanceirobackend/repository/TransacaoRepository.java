7171185615a1063ef1368c23f9161d06c287883b
alexandre.villela <ap.villela10@gmail.com>
2026-06-05 21:13:18 -0300

    Revert "Merge pull request #143 from IFSC-ES2/feat/106-paginacao-filtros-transacoes"
    This reverts commit 2518421cd0383daf0273578d1a5104c9ff7bfb2f, reversing
    changes made to 0f75fa12e7978c28331801b6f84b1c5f25c07cba.

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/TransacaoRepository.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/TransacaoRepository.java
@@ -3,15 +3,13 @@ package bcd.appfinanceirobackend.repository;
 import bcd.appfinanceirobackend.model.Conta;
 import bcd.appfinanceirobackend.model.Transacao;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface TransacaoRepository
-        extends JpaRepository<Transacao, UUID>, JpaSpecificationExecutor<Transacao> {
+public interface TransacaoRepository extends JpaRepository<Transacao, UUID> {
     List<Transacao> findAllByContaUsuarioId(UUID usuarioId);
     boolean existsByContaId(UUID contaId);
 }
