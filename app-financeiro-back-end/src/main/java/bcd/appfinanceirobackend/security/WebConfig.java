0def47effba2c7d9c0cd0dbe3bdbcb75dcd52512
Victor Lacerda <victor.g03@aluno.ifsc.edu.br>
2026-06-09 19:08:01 -0300

    fix: corrige cors para frontend em producao

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/security/WebConfig.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/security/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-         
-                .allowedOrigins("http://localhost:5173")
+                .allowedOrigins(allowedOrigins)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
