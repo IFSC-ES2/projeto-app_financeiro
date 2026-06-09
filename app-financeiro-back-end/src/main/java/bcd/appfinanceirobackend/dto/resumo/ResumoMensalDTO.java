5d8feb44f5012b05e93cfe40e08c450729f93372
Victor3294 <vblum26@gmail.com>
2026-06-09 19:48:31 -0300

    feat: Criando o resumoMensalDTO

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/resumo/ResumoMensalDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/resumo/ResumoMensalDTO.java
@@ -1,4 +1,25 @@
 package bcd.appfinanceirobackend.dto.resumo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class ResumoMensalDTO {
+    private int ano;
+    private int mes;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+    private BigDecimal totalRecebido;
+    private BigDecimal totalGasto;
+    private BigDecimal saldo;
+    private UUID categoriaMaiorGastoId;
+    private String categoriaMaiorGastoNome;
+    private BigDecimal categoriaMaiorGastoTotal;
+    private BigDecimal variacaoPercentualGastos;
+    private Boolean possuiTransacoes;
 }
