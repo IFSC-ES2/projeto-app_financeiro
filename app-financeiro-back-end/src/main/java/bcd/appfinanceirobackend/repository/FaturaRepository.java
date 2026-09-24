492adc9939b0dc1098e8a5dfcdcef40aa0616f71
Victor3294 <vblum26@gmail.com>
2026-09-23 21:14:31 -0300

    feat: Implementando funções auxiliares no FaturaRepository

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/FaturaRepository.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/repository/FaturaRepository.java
@@ -4,8 +4,13 @@ import bcd.appfinanceirobackend.model.Fatura;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.YearMonth;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface FaturaRepository extends JpaRepository<Fatura, UUID> {
+    List<Fatura> findByContaOrderByMesReferenciaDesc(UUID contaId);
+    Optional<Fatura> findByContaIdAndMesReferencia(UUID contaId, YearMonth mesReferencia);
 }
