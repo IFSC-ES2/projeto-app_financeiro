d4cf0a1d1dfe635e518c4c13d90bb7c168684a96
Victor3294 <vblum26@gmail.com>
2026-09-23 21:10:17 -0300

    feat: Criando o faturaResumoDTO

--- app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/fatura/FaturaResumoDTO.java
+++ app-financeiro-back-end/src/main/java/bcd/appfinanceirobackend/dto/fatura/FaturaResumoDTO.java
@@ -1,4 +1,19 @@
 package bcd.appfinanceirobackend.dto.fatura;
 
+import bcd.appfinanceirobackend.model.enums.StatusFatura;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Getter
+@Setter
 public class FaturaResumoDTO {
+    private UUID faturaId;
+    private String nomeConta;
+    private LocalDate dataVencimento;
+    private BigDecimal valorTotal;
+    private StatusFatura status;
 }
